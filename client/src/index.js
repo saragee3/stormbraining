4e2525f95c4606663181ea55a30b876cbe9d3429
Kevin Chen <kevnchn@gmail.com>
2016-04-29 17:08:46 -0400

    (chore) Strip down client boilerplate and remove jQuery

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { Router, useRouterHistory } from 'react-router';
-import { createHashHistory } from 'history';
+import { Router, Redirect, browserHistory as history } from 'react-router';
 import configureStore from './store/configureStore';
 import routes from './routes';
 
-const history = useRouterHistory(createHashHistory)({ queryKey: false });
 const store = configureStore();
 
 ReactDOM.render(
