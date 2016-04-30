ae265b35fea373a2862815eaddb5d88b71f454b4
fswiecki <frances.swiecki@gmail.com>
2016-04-30 18:03:56 -0400

    (feat) Add promise middleware and dependencies

--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Router, Route, browserHistory } from 'react-router';
+import promise from 'redux-promise';
 
 import App from './components/app';
 import Ideas from './components/ideas';
 import Boards from './components/boards';
 import reducers from './reducers';
 
-const store = createStore(reducers);
+const store = applyMiddleware(promise)(createStore);
 
 render(
-  <Provider store={ store }>
+  <Provider store={ store(reducers) }>
     <Router history={browserHistory}>
       <Route path="/" component={ App }>
         <Route path="/ideas" component={ Ideas } />
