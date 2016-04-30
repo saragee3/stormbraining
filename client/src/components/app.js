555c9da4eea019c6b3d0173caec7a658ca50259a
fswiecki <frances.swiecki@gmail.com>
2016-04-29 22:26:59 -0400

    (feat) Streamline navigation

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -10,11 +10,13 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <h1> Stormbrain </h1>
-        <ul>
-          <li><Link to="/boards">Boards</Link></li>
-          <li><Link to="/ideas">Ideas</Link></li>
-        </ul>
+        <nav className="navbar">
+          <Link to="/" className="navbar-brand">Stormbraining</Link>
+          <ul className="nav navbar-nav">
+            <li className="nav-item"><Link to="/boards">Boards</Link></li>
+            <li className="nav-item"><Link to="/ideas">Ideas</Link></li>
+          </ul>
+        </nav>
         {this.props.children}
       </div>
     );
