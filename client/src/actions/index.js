c93175682cfd27f6c767c599c0b3f000171e2eb1
fswiecki <frances.swiecki@gmail.com>
2016-04-30 20:35:02 -0400

    (feat) Create front end API endpoints for boards

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,19 +1,39 @@
+import axios from 'axios';
+
 export const NEW_IDEA = 'NEW_IDEA';
 export const NEW_BOARD = 'NEW_BOARD';
+export const GET_BOARDS = 'GET_BOARDS';
+export const GET_ONE_BOARD = 'GET_ONE_BOARD';
 
-let nextIdeaId = 0;
+const ROOT_URL = '/api';
 
 export function newIdea(idea) {
   return {
     type: NEW_IDEA,
-    id: nextIdeaId++,
     payload: idea,
   };
 }
 
 export function newBoard(title) {
+  const boardRequest = axios.post(ROOT_URL + '/boards', { title });
   return {
     type: NEW_BOARD,
-    payload: title,
+    payload: boardRequest,
+  };
+}
+
+export function getBoards() {
+  const request = axios.get(ROOT_URL + '/boards');
+  return {
+    type: GET_BOARDS,
+    payload: request,
+  };
+}
+
+export function getOneBoard(id) {
+  const request = axios.get(ROOT_URL + '/boards/' + id);
+  return {
+    type: GET_ONE_BOARD,
+    payload: request,
   };
 }
--- client/src/reducers/board.js
+++ client/src/reducers/board.js
@@ -1,12 +1,16 @@
-import { NEW_BOARD } from '../actions/index';
+import { NEW_BOARD, GET_BOARDS, GET_ONE_BOARD } from '../actions/index';
 
-const INITIAL_STATE = [];
+const INITIAL_STATE = { all: [], board: null };
 
 export default function (state = INITIAL_STATE, action) {
   switch (action.type) {
     case NEW_BOARD:
-      return [...state, action.payload];
-
+      const allBoards = state.all.concat(action.payload.data.board);
+      return { ...state, all: allBoards };
+    case GET_BOARDS:
+      return { ...state, all: action.payload.data.boards };
+    case GET_ONE_BOARD:
+      return { ...state, board: action.payload.data.board };
     default:
       return state;
   }
