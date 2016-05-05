26d62b123f077acf954e33dc2784f599c4b62584
Sara <sgee03@gmail.com>
2016-05-04 20:38:22 -0700

    (feat) Test routes

--- server/config/routes.js
+++ server/config/routes.js
@@ -24,8 +24,22 @@ export default function routes(app, express) {
   app.route('/api/boards/:board_id/ideas/:idea_id/upvotes')
     .post(IdeaController.upvoteIdea);
 
+  app.route('/sessions/create')
+    .post(function (req, res) {
+      if(req.body.password === "password"){
+        var token = {
+        id_token: {
+          username: req.body.username, 
+        },                                          
+      };
+        res.status(201).send(token);
+      } else {
+        res.status(400).send('Wrong password');
+      };
+    });
+
   app.route(/.*/)
     .get(function root(req, res) {
       res.sendFile(path.join(__dirname, '/../../client/index.html'));
-    });
+    });  
 }
