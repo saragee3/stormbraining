c52511a494819582c4c1faaa29ed4a1e3f1ad198
David Nguyen <davidnguyen21@gmail.com>
2016-05-09 17:06:02 +0900

    (feat) Use Auth0 Id

--- server/orm/models/User.js
+++ server/orm/models/User.js
@@ -3,7 +3,7 @@ import thinky from '../thinkyConfig.js';
 const r = thinky.r;
 const type = thinky.type;
 
-const User = thinky.createModel( 'User', {
+const User = thinky.createModel('User', {
   id: type.string(),
   name: type.string().required(),
   email: type.string().required(),
@@ -15,6 +15,5 @@ export default User;
 // require used instead of import due to same issue (https://github.com/neumino/thinky/issues/399)
 const Board = require('./Board').default;
 const Idea = require('./Idea').default;
-User.hasMany(Board, 'board', 'boardId', 'id');
-User.hasMany(Idea, 'ideas', 'id', 'boardId');
-
+User.hasMany(Board, 'authoredBoards', 'id', 'authorId');
+User.hasMany(Idea, 'ideas', 'id', 'authorId');
