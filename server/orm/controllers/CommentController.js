3b0d28d9385aec07e4170b65eee481fda899c19a
fswiecki <frances.swiecki@gmail.com>
2016-05-10 16:27:29 -0400

    (feat) Create server-side rotues for comments

---
+++ server/orm/controllers/CommentController.js
@@ -0,0 +1,53 @@
+import Comment from '../models/Comment.js';
+import helper from './helper.js';
+
+export default {
+  addComment: (req, res) => {
+    const ideaId = req.params.idea_id;
+    const { content, authorId } = req.body;
+    const newComment = new Comment({ content, ideaId, authorId });
+
+    newComment.save()
+      .then((comment) => {
+        res.status(201).json({ comment });
+      })
+      .error(helper.handleError(res));
+  },
+
+  deleteComment: (req, res) => {
+    const id = req.params.comment_id;
+    const userId = req.body.userId;
+
+    Comment.get(id).run()
+      .then((comment) => {
+        if (userId === comment.authorId) {
+          comment.delete()
+            .then((result) => {
+              res.sendStatus(204);
+            });
+        } else {
+          console.log('Permission denied.');
+        }
+      });
+  },
+
+  updateComment: (req, res) => {
+    const id = req.params.idea_id;
+    const update = req.body;
+    const userId = req.body.userId;
+
+    Comment.get(id).run()
+      .then((comment) => {
+        if (userId === comment.authorId) {
+          comment.merge(update).save()
+            .then((result) => {
+              res.status(200).json({ result });
+            });
+        } else {
+          console.log('Permission denied.');
+          throw new Error('Permission denied');
+        }
+      })
+      .error(helper.handleError(res));
+  },
+};
