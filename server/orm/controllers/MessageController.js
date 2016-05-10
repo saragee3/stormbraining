84128eac6b70e2fb4c9d5d9146014a38cf487a90
David Nguyen <davidnguyen21@gmail.com>
2016-05-10 14:31:07 +0900

    (feat) Add add/get queries for messages

---
+++ server/orm/controllers/MessageController.js
@@ -0,0 +1,26 @@
+import Message from '../models/Message.js';
+import helper from './helper.js';
+
+export default {
+  getMessages: (req, res) => {
+    const boardId = req.params.board_id;
+
+    Message.get(boardId).run()
+      .then((messages) => {
+        res.status(200).json({ messages });
+      })
+      .error(helper.handleError(res));
+  },
+
+  addMessage: (req, res) => {
+    const boardId = req.params.board_id;
+    const { userName, message } = req.body;
+    const newMessage = new Message({ userName, message, boardId });
+
+    newMessage.save()
+      .then((message) => {
+        res.status(201).json({ message });
+      })
+      .error(helper.handleError(res));
+  },
+};
