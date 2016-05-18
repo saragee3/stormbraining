29b7e9bb7e8f4200f979dc9b4d1f22484116bf6e
Sara <sgee03@gmail.com>
2016-05-17 22:28:45 -0700

    (feat) Create popover view for inputting emails

---
+++ client/src/containers/email.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { sendEmail } from '../actions/index';
+
+import FlatButton from 'material-ui/FlatButton';
+import RaisedButton from 'material-ui/RaisedButton';
+import Popover from 'material-ui/Popover';
+import TextField from 'material-ui/TextField';
+
+class Email extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = { open: false, term: '' };
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  onInputChange(event) {
+    this.setState({ term: event.target.value });
+  }
+
+  onFormSubmit(event) {
+    event.preventDefault();
+    sendEmail(this.state.term, `https://storm-braining.herokuapp.com/boards/${this.props.params.board_id}`);
+    this.setState({ term: '', open: false });
+  }
+
+  handleTouchTap = (event) => {
+    event.preventDefault();
+
+    this.setState({
+      open: true,
+      anchorEl: event.currentTarget,
+    });
+  }
+
+  handleRequestClose = () => {
+    this.setState({ open: false });
+  }
+
+  render() {
+    return (
+      <span>
+        <FlatButton
+          onTouchTap={this.handleTouchTap}
+          label="Email"
+        />
+        <Popover
+          style={{ padding: '5px', width: '25%' }}
+          open={this.state.open}
+          anchorEl={this.state.anchorEl}
+          anchorOrigin={{ horizontal: 'left', vertical: 'bottom' }}
+          targetOrigin={{ horizontal: 'left', vertical: 'top' }}
+          onRequestClose={this.handleRequestClose}
+        >
+          <form onSubmit={this.onFormSubmit}>
+            <TextField
+              style={{ padding: '10px', height: '100px' }}
+              floatingLabelText="e.g., johndoe@gmail.com, davie@yahoo.com, user@user.com"
+              hintText="Insert emails"
+              value={this.state.term}
+              onChange={this.onInputChange}
+            />
+            <RaisedButton
+              style={{ margin: '10px' }}
+              type="submit"
+              label="Submit"
+            />
+          </form>
+        </Popover>
+      </span>
+    );
+  }
+}
+
+export default connect(null, { sendEmail })(Email);
